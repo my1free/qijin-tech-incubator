06144c9072209d5ea80aa73d8ba9eaec530b39cf
michealyang <yangshangqiang@inke.cn>
2021-06-27 09:13:38 +0800

    init project

---
+++ social/src/main/java/tech/qijin/incubator/social/server/api/BaseController.java
@@ -0,0 +1,17 @@
+package tech.qijin.incubator.social.server.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author michealyang
+ * @date 2019-12-06
+ * @relax: 开始眼保健操 ←_← ↓_↓ →_→ ↑_↑
+ */
+@RestController
+public class BaseController {
+    @RequestMapping("/alive")
+    public String alive() {
+        return "OK";
+    }
+}
